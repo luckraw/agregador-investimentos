0ee21fe28e9d582cd2490955e978e634e405af15
nulld <krawdev@gmail.com>
2024-01-03 11:01:10 -0300

    project created

---
+++ src/test/java/com/luckraw/agregadorinvestimentos/AgregadorInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckraw.agregadorinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
