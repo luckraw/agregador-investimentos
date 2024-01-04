21f3e63c432e7451428abbb38ecfaab38a55f0de
nulld <krawdev@gmail.com>
2024-01-04 15:13:55 -0300

    feat: updateUserByInd unit test and fixes/refactor.

--- src/test/java/com/luckraw/agregadorinvestimentos/service/UserServiceTest.java
+++ src/test/java/com/luckraw/agregadorinvestimentos/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.luckraw.agregadorinvestimentos.service;
 
 import com.luckraw.agregadorinvestimentos.controller.CreateUserDTO;
+import com.luckraw.agregadorinvestimentos.controller.UpdateUserDTO;
 import com.luckraw.agregadorinvestimentos.entity.User;
 import com.luckraw.agregadorinvestimentos.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -13,14 +14,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -38,26 +40,32 @@ class UserServiceTest {
     private ArgumentCaptor<UUID> uuidArgumentCaptor;
 
     @Nested
-    class CreateUser {
+    class createUser {
 
         @Test
-        @DisplayName("should create user")
-        void shouldCreateUser() {
+        @DisplayName("Should create a user with success")
+        void shouldCreateAUserWithSuccess() {
 
+            // Arrange
             var user = new User(
                     UUID.randomUUID(),
                     "username",
-                    "email@example",
-                    "123456",
+                    "email@email.com",
+                    "password",
                     Instant.now(),
                     null
             );
-
             doReturn(user).when(userRepository).save(userArgumentCaptor.capture());
-            var input = new CreateUserDTO("username", "email@example", "123456");
+            var input = new CreateUserDTO(
+                    "username",
+                    "email@email.com",
+                    "123"
+            );
 
+            // Act
             var output = userService.createUser(input);
 
+            // Assert
             assertNotNull(output);
 
             var userCaptured = userArgumentCaptor.getValue();
@@ -68,56 +76,217 @@ class UserServiceTest {
         }
 
         @Test
-        @DisplayName("should throw exception when error occurs")
+        @DisplayName("Should throw exception when error occurs")
         void shouldThrowExceptionWhenErrorOccurs() {
-
-
-            doThrow(new RuntimeException()).when(userRepository).save(userArgumentCaptor.capture());
+            // Arrange
+            doThrow(new RuntimeException()).when(userRepository).save(any());
             var input = new CreateUserDTO(
                     "username",
-                    "email@example",
-                    "123456");
+                    "email@email.com",
+                    "123"
+            );
 
+            // Act & Assert
             assertThrows(RuntimeException.class, () -> userService.createUser(input));
-
         }
     }
 
     @Nested
-    class GetUserById {
+    class getUserById {
 
         @Test
-        @DisplayName("should get user by id with success when option is present")
-        void shouldGetUserByIdWithSuccessWhenOptionIsPresent() {
+        @DisplayName("Should get user by id with success when optional is present")
+        void shouldGetUserByIdWithSuccessWhenOptionalIsPresent() {
 
+            // Arrange
             var user = new User(
                     UUID.randomUUID(),
                     "username",
-                    "email@example",
-                    "123456",
+                    "email@email.com",
+                    "password",
                     Instant.now(),
                     null
             );
+            doReturn(Optional.of(user))
+                    .when(userRepository)
+                    .findById(uuidArgumentCaptor.capture());
 
-            doReturn(Optional.of(user)).when(userRepository).findById(uuidArgumentCaptor.capture());
-
+            // Act
             var output = userService.getUserById(user.getUserId().toString());
 
+            // Assert
             assertTrue(output.isPresent());
             assertEquals(user.getUserId(), uuidArgumentCaptor.getValue());
         }
 
         @Test
-        @DisplayName("should get user by id with success when option is empty")
-        void shouldGetUserByIdWithSuccessWhenOptionIsEmpty() {
+        @DisplayName("Should get user by id with success when optional is empty")
+        void shouldGetUserByIdWithSuccessWhenOptionalIsEmpty() {
 
+            // Arrange
             var userId = UUID.randomUUID();
-            doReturn(Optional.empty()).when(userRepository).findById(uuidArgumentCaptor.capture());
+            doReturn(Optional.empty())
+                    .when(userRepository)
+                    .findById(uuidArgumentCaptor.capture());
 
+            // Act
             var output = userService.getUserById(userId.toString());
 
+            // Assert
             assertTrue(output.isEmpty());
             assertEquals(userId, uuidArgumentCaptor.getValue());
+        }
+    }
+
+    @Nested
+    class listUsers {
+
+        @Test
+        @DisplayName("Should return all users with success")
+        void shouldReturnAllUsersWithSuccess() {
+
+            // Arrange
+            var user = new User(
+                    UUID.randomUUID(),
+                    "username",
+                    "email@email.com",
+                    "password",
+                    Instant.now(),
+                    null
+            );
+            var userList = List.of(user);
+            doReturn(userList)
+                    .when(userRepository)
+                    .findAll();
+
+            // Act
+            var output = userService.listUsers();
+
+            // Assert
+            assertNotNull(output);
+            assertEquals(userList.size(), output.size());
+        }
+    }
+
+    @Nested
+    class deleteById {
+
+        @Test
+        @DisplayName("Should delete user with success when user exists")
+        void shouldDeleteUserWithSuccessWhenUserExists() {
+
+            // Arrange
+            doReturn(true)
+                    .when(userRepository)
+                    .existsById(uuidArgumentCaptor.capture());
+
+            doNothing()
+                    .when(userRepository)
+                    .deleteById(uuidArgumentCaptor.capture());
+
+            var userId = UUID.randomUUID();
+
+            // Act
+            userService.deleteById(userId.toString());
+
+            // Assert
+            var idList = uuidArgumentCaptor.getAllValues();
+            assertEquals(userId, idList.get(0));
+            assertEquals(userId, idList.get(1));
+
+            verify(userRepository, times(1)).existsById(idList.get(0));
+            verify(userRepository, times(1)).deleteById(idList.get(1));
+        }
+
+        @Test
+        @DisplayName("Should not delete user when user NOT exists")
+        void shouldNotDeleteUserWhenUserNotExists() {
+
+            // Arrange
+            doReturn(false)
+                    .when(userRepository)
+                    .existsById(uuidArgumentCaptor.capture());
+            var userId = UUID.randomUUID();
+
+            // Act
+            userService.deleteById(userId.toString());
+
+            // Assert
+            assertEquals(userId, uuidArgumentCaptor.getValue());
+
+            verify(userRepository, times(1))
+                    .existsById(uuidArgumentCaptor.getValue());
+
+            verify(userRepository, times(0)).deleteById(any());
+        }
+    }
+
+    @Nested
+    class updateUserById {
+
+        @Test
+        @DisplayName("Should update user by id when user exists and username and password is filled")
+        void shouldUpdateUserByIdWhenUserExistsAndUsernameAndPasswordIsFilled() {
+
+            // Arrange
+            var updateUserDto = new UpdateUserDTO(
+                    "newUsername",
+                    "newPassword"
+            );
+            var user = new User(
+                    UUID.randomUUID(),
+                    "username",
+                    "email@email.com",
+                    "password",
+                    Instant.now(),
+                    null
+            );
+            doReturn(Optional.of(user))
+                    .when(userRepository)
+                    .findById(uuidArgumentCaptor.capture());
+            doReturn(user)
+                    .when(userRepository)
+                    .save(userArgumentCaptor.capture());
+
+            // Act
+            userService.updateUserById(user.getUserId().toString(), updateUserDto);
+
+            // Assert
+            assertEquals(user.getUserId(), uuidArgumentCaptor.getValue());
+
+            var userCaptured = userArgumentCaptor.getValue();
+
+            assertEquals(updateUserDto.username(), userCaptured.getUserName());
+            assertEquals(updateUserDto.password(), userCaptured.getPassword());
+
+            verify(userRepository, times(1))
+                    .findById(uuidArgumentCaptor.getValue());
+            verify(userRepository, times(1)).save(user);
+        }
+
+        @Test
+        @DisplayName("Should not update user when user not exists")
+        void shouldNotUpdateUserWhenUserNotExists() {
+
+            // Arrange
+            var updateUserDto = new UpdateUserDTO(
+                    "newUsername",
+                    "newPassword"
+            );
+            var userId = UUID.randomUUID();
+            doReturn(Optional.empty())
+                    .when(userRepository)
+                    .findById(uuidArgumentCaptor.capture());
+
+            // Act
+            userService.updateUserById(userId.toString(), updateUserDto);
+
+            // Assert
+            assertEquals(userId, uuidArgumentCaptor.getValue());
+
+            verify(userRepository, times(1))
+                    .findById(uuidArgumentCaptor.getValue());
+            verify(userRepository, times(0)).save(any());
         }
     }
 }
