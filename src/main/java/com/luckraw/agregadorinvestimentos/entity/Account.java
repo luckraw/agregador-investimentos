69e0183474f709ac3618b8eba881fb879b500982
nulld <krawdev@gmail.com>
2024-01-05 16:53:10 -0300

    feat: User and Account relationship.

---
+++ src/main/java/com/luckraw/agregadorinvestimentos/entity/Account.java
@@ -0,0 +1,53 @@
+package com.luckraw.agregadorinvestimentos.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountId;
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Account() {
+
+    }
+
+    public Account(UUID accountId, String description) {
+        this.accountId = accountId;
+        this.description = description;
+    }
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(UUID accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
