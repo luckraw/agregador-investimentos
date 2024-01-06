2bdb5622a3d3265c8b8353ab0aa5bafb38ea8e94
nulld <krawdev@gmail.com>
2024-01-06 00:25:05 -0300

    feat: AccountController and Service POST AssociateStock impl.

---
+++ src/main/java/com/luckraw/agregadorinvestimentos/controller/AccountController.java
@@ -0,0 +1,24 @@
+package com.luckraw.agregadorinvestimentos.controller;
+
+import com.luckraw.agregadorinvestimentos.controller.dto.AssociateAccountStockDTO;
+import com.luckraw.agregadorinvestimentos.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/accounts")
+public class AccountController {
+
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/{accountId}/stocks")
+    public ResponseEntity<Void> associateStock(@PathVariable("accountId") String accountId, @RequestBody AssociateAccountStockDTO associateAccountStockDTO) {
+        accountService.associateStock(accountId, associateAccountStockDTO);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/luckraw/agregadorinvestimentos/service/AccountService.java
@@ -0,0 +1,45 @@
+package com.luckraw.agregadorinvestimentos.service;
+
+import com.luckraw.agregadorinvestimentos.controller.dto.AssociateAccountStockDTO;
+import com.luckraw.agregadorinvestimentos.entity.AccountStock;
+import com.luckraw.agregadorinvestimentos.entity.AccountStockId;
+import com.luckraw.agregadorinvestimentos.repository.AccountRepository;
+import com.luckraw.agregadorinvestimentos.repository.AccountStockRepository;
+import com.luckraw.agregadorinvestimentos.repository.StockRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+public class AccountService {
+
+    private AccountRepository accountRepository;
+    private StockRepository stockRepository;
+    private AccountStockRepository accountStockRepository;
+
+    public AccountService(AccountRepository accountRepository, StockRepository stockRepository, AccountStockRepository accountStockRepository) {
+        this.accountRepository = accountRepository;
+        this.stockRepository = stockRepository;
+        this.accountStockRepository = accountStockRepository;
+    }
+
+    public void associateStock(String accountId, AssociateAccountStockDTO associateAccountStockDTO) {
+
+        var account = accountRepository.findById(UUID.fromString(accountId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var stock = stockRepository.findById(associateAccountStockDTO.stockId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var id = new AccountStockId(account.getAccountId(), stock.getStockId());
+        var entity = new AccountStock(id,
+                account,
+                stock,
+                associateAccountStockDTO.quantity());
+
+        accountStockRepository.save(entity);
+
+    }
+}
