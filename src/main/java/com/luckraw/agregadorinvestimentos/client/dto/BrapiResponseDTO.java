8bd76555af86c41c0d173b6c7af1225cdecb3f3e
nulld <krawdev@gmail.com>
2024-01-06 11:08:08 -0300

    feat: DTOs, OpenFeign API Configuration and Impl.

---
+++ src/main/java/com/luckraw/agregadorinvestimentos/client/dto/BrapiResponseDTO.java
@@ -0,0 +1,6 @@
+package com.luckraw.agregadorinvestimentos.client.dto;
+
+import java.util.List;
+
+public record BrapiResponseDTO(List<StockDTO> results) {
+}
---
+++ src/main/java/com/luckraw/agregadorinvestimentos/client/dto/StockDTO.java
@@ -0,0 +1,4 @@
+package com.luckraw.agregadorinvestimentos.client.dto;
+
+public record StockDTO(double regularMarketPrice) {
+}
