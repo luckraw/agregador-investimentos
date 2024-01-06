f4f4dad2ed47e2ba8b4c4276725e0c66ce57e555
nulld <krawdev@gmail.com>
2024-01-05 22:27:37 -0300

    feat: CreateUserDTO.

--- src/main/java/com/luckraw/agregadorinvestimentos/controller/dto/CreateUserDTO.java
+++ src/main/java/com/luckraw/agregadorinvestimentos/controller/dto/CreateUserDTO.java
@@ -1,4 +1,4 @@
-package com.luckraw.agregadorinvestimentos.controller;
+package com.luckraw.agregadorinvestimentos.controller.dto;
 
 public record CreateUserDTO(String username, String email, String password) {
 }
--- src/main/java/com/luckraw/agregadorinvestimentos/controller/dto/UpdateUserDTO.java
+++ src/main/java/com/luckraw/agregadorinvestimentos/controller/dto/UpdateUserDTO.java
@@ -1,4 +1,4 @@
-package com.luckraw.agregadorinvestimentos.controller;
+package com.luckraw.agregadorinvestimentos.controller.dto;
 
 public record UpdateUserDTO(String username, String password) {
 }
